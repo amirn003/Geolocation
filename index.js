d3857f3db89eddc0930b157a915d78daa5b7fc6f
amirn003 <amir.naar@gmail.com>
2023-11-16 19:07:34 +0100

    catch speed from geolocation, update the compass accordingly

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((data) => {
   console.log(data);
+  speed.textContent = data.coords.speed;
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+  console.err(err);
+  alert('Hey! You gotta allow to that to happen!')
 });
